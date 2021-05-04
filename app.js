de34ccb7be9e8f9addf314e85a9b7ab4ef00d153
Arihant Jain <arihant427@gmail.com>
2021-05-04 21:29:06 +0530

    First commit

--- app.js
+++ app.js
@@ -4,30 +4,6 @@ var fs = require("fs");
 
 
 
-
-app.get('/getdata', function (req, res) {
- var request = require("request");
-    var options = { method: 'GET',
-    url: 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=301001&date=03-05-2021',
-   
- }
- 
-
- 
- request(options, function (error, response, body) {
-        if (error) {
-          console.error(error);
-                        res.status(200).send(error);
-
-        }
-        else{
-          console.log(response);
-        }
-              res.status(200).send(response);
-});
-
-});
-
 app.get('/', function (req, res){
               res.status(200).send({});
 });
