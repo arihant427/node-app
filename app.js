3ee17b03043f72638392ce969e71eff78f9bface
Arihant Jain <arihant427@gmail.com>
2021-05-04 21:04:39 +0530

    First commit

---
+++ app.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/getdata', function (req, res) {
+ var request = require("request");
+    var options = { method: 'GET',
+    url: 'https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=301001&date=03-05-2021',
+   
+ }
+ 
+ request(options, function (error, response, body) {
+        if (error) {
+          console.error(error);
+                        res.status(200).send(error);
+
+        }
+        else{
+          console.log(response);
+        }
+              res.status(200).send(response);
+});
+
+});
+
+var server = app.listen(8081, function () {
+   var host = server.address().address
+   var port = server.address().port
+   console.log("Example app listening at http://%s:%s", host, port)
+})
